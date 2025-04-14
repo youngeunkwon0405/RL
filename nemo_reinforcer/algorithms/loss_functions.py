10a80f15ea6f013f0568379f168d20af63d201d7
Gerald Shen <119401249+gshennvm@users.noreply.github.com>
2025-04-14 14:51:26 -0700

    Update nemo_reinforcer/algorithms/loss_functions.py
    Co-authored-by: Parth Chadha <pchadha@nvidia.com>

--- nemo_reinforcer/algorithms/loss_functions.py
+++ nemo_reinforcer/algorithms/loss_functions.py
@@ -114,7 +114,7 @@ class ClippedPGLossFn(LossFunction):
             next_token_logprobs = torch.nn.functional.log_softmax(
                 next_token_logits, dim=-1
             )
-            next_tokens = data["input_ids"][:, 1:]  # Skip first tokebatch_size, 1)
+            next_tokens = data["input_ids"][:, 1:]  # Skip first token
             curr_logprobs = next_token_logprobs.gather(
                 dim=-1, index=next_tokens.unsqueeze(-1)
             ).squeeze(-1)
