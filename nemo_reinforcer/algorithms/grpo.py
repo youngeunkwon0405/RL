52fc721cfe2ae23dbefb007ddf613a2ad060451e
Gerald Shen <geshen@nvidia.com>
2025-04-15 12:30:20 -0700

    make_divisible_by -> make_sequence_length_divisible_by
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_reinforcer/algorithms/grpo.py
+++ nemo_reinforcer/algorithms/grpo.py
@@ -543,7 +543,7 @@ def grpo_train(
                 flat_messages, input_lengths = batched_message_log_to_flat_message(
                     repeated_batch["message_log"],
                     pad_value_dict={"token_ids": tokenizer.pad_token_id},
-                    make_divisible_by=master_config["policy"][
+                    make_sequence_length_divisible_by=master_config["policy"][
                         "make_sequence_length_divisible_by"
                     ],
                 )
