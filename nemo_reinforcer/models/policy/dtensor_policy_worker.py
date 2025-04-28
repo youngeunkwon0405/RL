fix normalization when rollout bs > gbs
Signed-off-by: ashors1 <ashors@nvidia.com>

nemo_reinforcer/models/policy/dtensor_policy_worker.py
@@ -290,22 +290,27 @@ class DTensorPolicyWorker:
             losses = []
             all_mb_metrics = []
             for gb_start in range(0, dataset_size, local_gbs):
-                assert "sample_mask" in data, "sample_mask must be present in the data!"
+                global_batch = data.slice(gb_start, gb_start + local_gbs)
+
+                assert "sample_mask" in global_batch, (
+                    "sample_mask must be present in the data!"
+                )
                 ## get the normalization factor for the loss
                 if loss_fn.loss_type == LossType.TOKEN_LEVEL:
-                    assert "token_mask" in data, (
+                    assert "token_mask" in global_batch, (
                         "token_mask must be present in the data when using token-level loss"
                     )
                     ## get number of tokens in the global batch
                     normalization_factor = torch.sum(
-                        data["token_mask"][:, 1:] * data["sample_mask"].unsqueeze(-1)
+                        global_batch["token_mask"][:, 1:]
+                        * global_batch["sample_mask"].unsqueeze(-1)
                     )
                     torch.distributed.all_reduce(
                         normalization_factor, group=self.dp_mesh.get_group()
                     )
                 elif loss_fn.loss_type == LossType.SAMPLE_LEVEL:
                     ## get number of valid samples in the global batch
-                    normalization_factor = torch.sum(data["sample_mask"])
+                    normalization_factor = torch.sum(global_batch["sample_mask"])
                     torch.distributed.all_reduce(
                         normalization_factor, group=self.dp_mesh.get_group()
                     )
@@ -320,9 +325,7 @@ class DTensorPolicyWorker:
                 # so its safe to not check for the case where the last data slice is smaller than mbs
                 num_microbatches = min(local_gbs, dataset_size - gb_start) // mbs
 
-                for mb in data.slice(
-                    gb_start, gb_start + local_gbs
-                ).make_microbatch_iterator(mbs):
+                for mb in global_batch.make_microbatch_iterator(mbs):
                     input_ids = mb.get("input_ids").cuda()
 
                     input_lengths = mb.get("input_lengths")