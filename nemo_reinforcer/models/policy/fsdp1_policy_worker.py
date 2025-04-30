another attention mask fix

nemo_reinforcer/models/policy/fsdp1_policy_worker.py
@@ -273,7 +273,7 @@ class FSDP1PolicyWorker:
 
                     input_lengths = mb.get("input_lengths")
                     batch_size, seq_len = input_ids.shape
-                    attention_mask = torch.ones(
+                    attention_mask = torch.zeros(
                         (batch_size, seq_len), dtype=torch.long, device=input_ids.device
                     )
                     for i, length in enumerate(input_lengths):