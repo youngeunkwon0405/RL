a70e335fc606feab9e9d9663f49697d107261400
Yi-Fu Wu <yifu.wu@gmail.com>
2025-04-30 12:43:29 -0700

    ruff
    Signed-off-by: Yi-Fu Wu <yifu.wu@gmail.com>

--- nemo_rl/models/dtensor/parallelize.py
+++ nemo_rl/models/dtensor/parallelize.py
@@ -29,10 +29,14 @@ from torch.distributed.tensor.parallel import (
 )
 from torch.distributed.tensor.placement_types import Replicate, Shard
 from transformers.models.llama.modeling_llama import LlamaForCausalLM
-from transformers.models.gemma3.modeling_gemma3 import Gemma3ForCausalLM, Gemma3ForConditionalGeneration
+from transformers.models.gemma3.modeling_gemma3 import (
+    Gemma3ForCausalLM,
+    Gemma3ForConditionalGeneration,
+)
 from transformers.models.qwen2.modeling_qwen2 import Qwen2ForCausalLM
 
 from nemo_rl.distributed.model_utils import from_parallel_logits_to_logprobs
+
 
 def _parallelize_gemma3(
     model: Union[Gemma3ForCausalLM, Gemma3ForConditionalGeneration],
