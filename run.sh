4beaa66dfddf70caad017066faab924648ab5e34
Terry Kong <terryk@nvidia.com>
2025-05-02 09:26:33 -0700

    clean
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- run.sh
+++
@@ -1,3 +0,0 @@
-
-set -ex
-./build.sh; rm -rf venvs/ ; uv run examples/run_grpo_math.py --config=examples/configs/grpo_math_1B_megatron.yaml cluster.gpus_per_node=2 logger.wandb.name=tmp_testing_megatron_2gpus logger.wandb_enabled=false 2>&1 | tee mcore.log
