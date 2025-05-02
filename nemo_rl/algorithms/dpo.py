cleanup, fix logging of normalization factor
Signed-off-by: ashors1 <ashors@nvidia.com>

nemo_rl/algorithms/dpo.py
@@ -302,7 +302,7 @@ def validate(
 
             else:
                 for k, v in val_results["all_mb_metrics"].items():
-                    if k == "lr":
+                    if k in {"lr", "normalization_factor"}:
                         val_metrics[k] += np.mean(v).item()
                     else:
                         val_metrics[k] += np.sum(v).item()
@@ -482,7 +482,7 @@ def dpo_train(
             }
             metrics.update(train_results["all_mb_metrics"])
             for k, v in metrics.items():
-                if k == "lr":
+                if k in {"lr", "normalization_factor"}:
                     metrics[k] = np.mean(v).item()
                 else:
                     metrics[k] = np.sum(v).item()

nemo_rl/algorithms/sft.py
@@ -481,7 +481,7 @@ def sft_train(
             }
             metrics.update(train_results["all_mb_metrics"])
             for k, v in metrics.items():
-                if k == "lr":
+                if k in {"lr", "normalization_factor"}:
                     metrics[k] = np.mean(v).item()
                 else:
                     metrics[k] = np.sum(v).item()

nemo_rl/models/policy/dtensor_policy_worker.py
@@ -309,7 +309,7 @@ class DTensorPolicyWorker:
                     torch.distributed.all_reduce(
                         total_valid_tokens_or_seqs, group=self.dp_mesh.get_group()
                     )
-                elif loss_fn.loss_type == LossType.SAMPLE_LEVEL:
+                elif loss_fn.loss_type == LossType.SEQUENCE_LEVEL:
                     ## get number of valid samples in the global batch
                     total_valid_tokens_or_seqs = torch.sum(global_batch["sample_mask"])
                     torch.distributed.all_reduce(
@@ -372,8 +372,8 @@ class DTensorPolicyWorker:
                     num_valid_samples = loss_metrics["num_valid_samples"]
                     loss_metrics["lr"] = self.optimizer.param_groups[0]["lr"]
                     loss_metrics["normalization_factor"] = (
-                        total_valid_tokens_or_seqs / self.dp_size
-                    ).cpu()
+                        total_valid_tokens_or_seqs.cpu()
+                    )
 
                     # Backward pass
                     if not eval_mode:

nemo_rl/models/policy/fsdp1_policy_worker.py
@@ -276,7 +276,7 @@ class FSDP1PolicyWorker:
                         * global_batch["sample_mask"].unsqueeze(-1)
                     )
                     torch.distributed.all_reduce(total_valid_tokens_or_seqs)
-                elif loss_fn.loss_type == LossType.SAMPLE_LEVEL:
+                elif loss_fn.loss_type == LossType.SEQUENCE_LEVEL:
                     ## get number of valid samples in the global batch
                     total_valid_tokens_or_seqs = torch.sum(global_batch["sample_mask"])
                     torch.distributed.all_reduce(total_valid_tokens_or_seqs)
@@ -323,8 +323,8 @@ class FSDP1PolicyWorker:
                     num_valid_samples = loss_metrics["num_valid_samples"]
                     loss_metrics["lr"] = self.optimizer.param_groups[0]["lr"]
                     loss_metrics["normalization_factor"] = (
-                        total_valid_tokens_or_seqs / torch.distributed.get_world_size()
-                    ).cpu()
+                        total_valid_tokens_or_seqs.cpu()
+                    )
 
                     # Backward pass
                     if not eval_mode: