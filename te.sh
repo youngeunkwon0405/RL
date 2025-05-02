6b7ed90c34cb164714cab2257db5056ddde33ad5
Terry Kong <terryk@nvidia.com>
2025-05-02 00:06:10 -0700

    make it better
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- te.sh
+++ te.sh
@@ -1,7 +1,34 @@
-export PATH=/usr/bin:$PATH
-set -xeu
+#!/bin/bash
+set -eoux pipefail
 
-rm -rf .venv
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+cd $SCRIPT_DIR
+
 uv sync
-uv sync --extra mcore
-uv run te.py
+uv run --extra mcore python <<"EOF"
+import torch
+import transformer_engine.pytorch as te
+from transformer_engine.common import recipe
+
+# Set dimensions.
+in_features = 768
+out_features = 3072
+hidden_size = 2048
+
+# Initialize model and inputs.
+model = te.Linear(in_features, out_features, bias=True)
+inp = torch.randn(hidden_size, in_features, device="cuda")
+
+# Create an FP8 recipe. Note: All input args are optional.
+fp8_recipe = recipe.DelayedScaling(margin=0, fp8_format=recipe.Format.E4M3)
+
+# Enable autocasting for the forward pass
+with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
+    out = model(inp)
+
+loss = out.sum()
+loss.backward()
+print("TE script finished")
+EOF
+uv sync
+echo Success
