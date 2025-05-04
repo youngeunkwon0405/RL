f8abab66c56dc571ee93c4fcd4e74032f0e44aa3
Terry Kong <terryk@nvidia.com>
2025-05-04 15:12:40 -0700

    remove fp8 from test
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- tests/functional/test_mcore_extra_installed_correctly.sh
+++ tests/functional/test_mcore_extra_installed_correctly.sh
@@ -19,12 +19,15 @@ hidden_size = 2048
 model = te.Linear(in_features, out_features, bias=True)
 inp = torch.randn(hidden_size, in_features, device="cuda")
 
-# Create an FP8 recipe. Note: All input args are optional.
-fp8_recipe = recipe.DelayedScaling(margin=0, fp8_format=recipe.Format.E4M3)
+# TODO: Disabling FP8 testing since CI machines may not support FP8
+## Create an FP8 recipe. Note: All input args are optional.
+#fp8_recipe = recipe.DelayedScaling(margin=0, fp8_format=recipe.Format.E4M3)
+#
+## Enable autocasting for the forward pass
+#with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
+#    out = model(inp)
 
-# Enable autocasting for the forward pass
-with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
-    out = model(inp)
+out = model(inp)
 
 loss = out.sum()
 loss.backward()
