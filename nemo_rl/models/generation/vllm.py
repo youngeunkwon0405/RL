0a674bbb37730337c8d06349048956ca4a422584
Parth Chadha <pchadha@nvidia.com>
2025-05-12 13:19:31 -0700

    fix: fix issues preventing running grpo on volta (#294)
    Signed-off-by: Parth Chadha <pchadha@nvidia.com>

--- nemo_rl/models/generation/vllm.py
+++ nemo_rl/models/generation/vllm.py
@@ -187,8 +187,9 @@ class VllmGenerationWorker:
             skip_tokenizer_init=self.cfg["vllm_cfg"]["skip_tokenizer_init"],
             tensor_parallel_size=self.cfg["vllm_cfg"]["tensor_parallel_size"],
             gpu_memory_utilization=self.cfg["vllm_cfg"]["gpu_memory_utilization"],
-            enable_prefix_caching=True,
-            dtype="auto",
+            # Disable prefix caching for devices with compute capability < 8 (Volta) due to vllm segfault.
+            enable_prefix_caching=torch.cuda.get_device_capability()[0] >= 8,
+            dtype=self.cfg["vllm_cfg"]["precision"],
             seed=seed,
             # Don't use cuda-graph by default as it leads to convergence issue (see https://github.com/NVIDIA/NeMo-RL/issues/186)
             enforce_eager=True,
