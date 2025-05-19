5275195a19c09332d8579556f95fdc33def3cd8a
Terry Kong <terrycurtiskong@gmail.com>
2025-05-19 14:11:47 -0700

    fix: enable dashboard on local clusters (#411)
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- nemo_rl/distributed/virtual_cluster.py
+++ nemo_rl/distributed/virtual_cluster.py
@@ -139,7 +139,7 @@ def init_ray(log_dir: Optional[str] = None):
 
     ray.init(
         log_to_driver=True,
-        include_dashboard=False,
+        include_dashboard=True,
         runtime_env=local_runtime_env,
         _temp_dir=os.path.abspath(log_dir) if log_dir else None,
         resources={cvd_tag: 1},
