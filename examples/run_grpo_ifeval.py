b8d3bef2d2a7b54aba59cca03f23d5afc8b6437d
abukharin-nv <abukharin@nvidia.com>
2025-05-19 07:00:34 -0700

    Add IFEval Environment
    Signed-off-by: abukharin-nv <abukharin@nvidia.com>

---
+++ examples/run_grpo_ifeval.py
@@ -0,0 +1,246 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import os
+import pprint
+import random
+from copy import deepcopy
+from dataclasses import dataclass
+
+import jsonlines
+from omegaconf import OmegaConf
+from transformers import AutoTokenizer
+
+from nemo_rl.algorithms.grpo import MasterConfig, grpo_train, setup
+from nemo_rl.algorithms.utils import get_tokenizer
+from nemo_rl.data import DataConfig
+from nemo_rl.data.interfaces import DatumSpec
+from nemo_rl.distributed.virtual_cluster import init_ray
+from nemo_rl.environments.ifeval_environment import IFEvalEnvironment
+from nemo_rl.models.generation.interfaces import configure_generation_config
+from nemo_rl.utils.config import load_config, parse_hydra_overrides
+from nemo_rl.utils.logger import get_next_experiment_dir
+
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Run GRPO training with configuration")
+    parser.add_argument(
+        "--config", type=str, default=None, help="Path to YAML config file"
+    )
+
+    # Parse known args for the script
+    args, overrides = parser.parse_known_args()
+
+    return args, overrides
+
+
+# ===============================================================================
+#                             Math Data Processor
+# ===============================================================================
+
+
+@dataclass
+class JsonlinesDataset:
+    jsonl_path: str
+    shuffle: bool
+    seed: int
+    tokenizer: AutoTokenizer
+    max_seq_length: int
+    filter_long_samples: bool = False
+
+    def __post_init__(self):
+        self.data = self._load_data()
+
+        idx_to_ignore = set()
+        if self.filter_long_samples:
+            for i, item in enumerate(self):
+                if item["length"] > self.max_seq_length:
+                    idx_to_ignore.add(i)
+            print(f"found {len(idx_to_ignore)} long samples to ignore on dataset init")
+
+        self.data = [item for i, item in enumerate(self.data) if i not in idx_to_ignore]
+
+        if self.shuffle:
+            rng = random.Random(self.seed)
+            rng.shuffle(self.data)
+
+    def _load_data(self):
+        with jsonlines.open(self.jsonl_path, "r") as reader:
+            data = [line for line in reader]
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx: int) -> DatumSpec:
+        data = self.data[idx]
+        # support single turn for now
+        assert len(data["messages"]) == 1
+        single_message = data["messages"][0]
+
+        message_log = []
+
+        # this will also contain system prompt
+        user_message = {"role": "user"}
+
+        for m in single_message:
+            if m["role"] == "user":
+                # need to be deepcopy to avoid overwriting the original metadata
+                extra_env_info = deepcopy(m["metadata"])
+
+        message = self.tokenizer.apply_chat_template(
+            single_message,
+            tokenize=False,
+            add_generation_prompt=True,
+            add_special_tokens=False,
+        )
+        user_message["token_ids"] = self.tokenizer.apply_chat_template(
+            single_message,
+            tokenize=True,
+            add_generation_prompt=True,
+            add_special_tokens=False,
+            return_tensors="pt",
+        )[0]
+        user_message["content"] = message
+        message_log.append(user_message)
+
+        length = sum(len(m["token_ids"]) for m in message_log)
+
+        output = {
+            "message_log": message_log,
+            "length": length,
+            "extra_env_info": extra_env_info,
+            "loss_multiplier": 1.0,
+            "idx": idx,
+            "task_name": data["task_name"],
+            "dataset": data["dataset"],
+        }
+
+        return output
+
+
+def setup_data(tokenizer: AutoTokenizer, data_config: DataConfig, env_configs):
+    print("\n▶ Setting up data...")
+
+    train_ds = JsonlinesDataset(
+        data_config["train"]["jsonl_path"],
+        data_config["train"]["shuffle"],
+        data_config["train"]["seed"],
+        tokenizer,
+        max_seq_length=data_config["max_input_seq_length"],
+        filter_long_samples=data_config["train"]["filter_long_samples"],
+    )
+    val_ds = JsonlinesDataset(
+        data_config["val"]["jsonl_path"],
+        data_config["val"]["shuffle"],
+        data_config["val"]["seed"],
+        tokenizer,
+        max_seq_length=data_config["max_input_seq_length"],
+        filter_long_samples=data_config["val"]["filter_long_samples"],
+    )
+
+    ifeval_env = IFEvalEnvironment.options(
+        runtime_env={
+            "py_executable": IFEvalEnvironment.DEFAULT_PY_EXECUTABLE,
+            "env_vars": dict(os.environ),  # Pass thru all user environment variables
+        }
+    ).remote(env_configs["ifeval"])
+
+    task_to_env = {}
+    task_to_env["ifeval"] = ifeval_env
+
+    return train_ds, val_ds, task_to_env, task_to_env
+
+
+def main():
+    """Main entry point."""
+    # Parse arguments
+    args, overrides = parse_args()
+
+    if not args.config:
+        args.config = os.path.join(
+            os.path.dirname(__file__), "configs", "grpo_math_1B.yaml"
+        )
+
+    config = load_config(args.config)
+    print(f"Loaded configuration from: {args.config}")
+
+    if overrides:
+        print(f"Overrides: {overrides}")
+        config = parse_hydra_overrides(config, overrides)
+
+    config: MasterConfig = OmegaConf.to_container(config, resolve=True)
+    print("Applied CLI overrides")
+
+    # Print config
+    print("Final config:")
+    pprint.pprint(config)
+
+    # Get the next experiment directory with incremented ID
+    config["logger"]["log_dir"] = get_next_experiment_dir(config["logger"]["log_dir"])
+    print(f"📊 Using log directory: {config['logger']['log_dir']}")
+    if config["checkpointing"]["enabled"]:
+        print(
+            f"📊 Using checkpoint directory: {config['checkpointing']['checkpoint_dir']}"
+        )
+
+    init_ray()
+
+    # setup tokenizer
+    tokenizer = get_tokenizer(config["policy"]["tokenizer"])
+    config["policy"]["generation"] = configure_generation_config(
+        config["policy"]["generation"], tokenizer
+    )
+
+    # setup data
+    (
+        dataset,
+        val_dataset,
+        task_to_env,
+        val_task_to_env,
+    ) = setup_data(tokenizer, config["data"], config["env"])
+
+    (
+        policy,
+        policy_generation,
+        cluster,
+        dataloader,
+        val_dataloader,
+        loss_fn,
+        logger,
+        checkpointer,
+        grpo_state,
+        master_config,
+    ) = setup(config, tokenizer, dataset, val_dataset)
+
+    grpo_train(
+        policy,
+        policy_generation,
+        dataloader,
+        val_dataloader,
+        tokenizer,
+        loss_fn,
+        task_to_env,
+        val_task_to_env,
+        logger,
+        checkpointer,
+        grpo_state,
+        master_config,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ nemo_rl/environments/ifeval_environment.py
@@ -0,0 +1,267 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import contextlib
+import io
+import logging
+from typing import Dict, List, Optional, Tuple, TypedDict
+
+import ray
+import torch
+
+from nemo_rl.distributed.batched_data_dict import BatchedDataDict
+from nemo_rl.distributed.virtual_cluster import PY_EXECUTABLES
+from nemo_rl.environments.instruction_following.instructions_registry import (
+    INSTRUCTION_DICT,
+)
+from nemo_rl.environments.interfaces import (
+    EnvironmentInterface,
+    EnvironmentReturn,
+)
+from nemo_rl.environments.metrics import (
+    calculate_pass_rate_per_prompt,
+)
+from nemo_rl.environments.utils import chunk_list_to_workers
+
+
+class IFEvalEnvConfig(TypedDict):
+    num_workers: int
+    stop_strings: Optional[List[str]] = None  # Default stop strings for this env
+
+
+def instruction_following_rewards(prompt, response, args):
+    """Tests response to see if instrutions are followed."""
+    try:
+        task_args = args
+        instruction_list = task_args["instruction_id_list"]
+        is_following_list = []
+
+        for index, instruction_id in enumerate(instruction_list):
+            try:
+                instruction_cls = INSTRUCTION_DICT[instruction_id]
+                instruction = instruction_cls(instruction_id)
+
+                kwargs = (
+                    task_args["instruction_kwargs"][index]
+                    if task_args["instruction_kwargs"][index] is not None
+                    else {}
+                )
+                instruction.build_description(**kwargs)
+                instruction_args = instruction.get_instruction_args()
+                if instruction_args and "prompt" in instruction_args:
+                    instruction.build_description(prompt=prompt)
+
+                if response.strip() and instruction.check_following(response):
+                    is_following_list.append(True)
+                else:
+                    is_following_list.append(False)
+            except Exception as e:
+                print(f"Error in instruction_following_rewards: {e}, task: {args}")
+
+        low, high = 0, 1
+        correctness = sum(is_following_list) / len(is_following_list)
+        score = low + (high - low) * correctness
+        return score, True
+    except Exception as e:
+        print(f"Error in instruction_following_rewards: {e}")
+        return 0, False
+
+
+@contextlib.contextmanager
+def _mute_output():
+    devnull_out, devnull_err = io.StringIO(), io.StringIO()
+    with (
+        contextlib.redirect_stdout(devnull_out),
+        contextlib.redirect_stderr(devnull_err),
+    ):
+        yield
+
+
+class IFEvalEnvironmentMetadata(TypedDict):
+    instruction_id_list: list
+    instruction_kwargs: list
+
+
+@ray.remote
+class HFVerifyWorker:
+    DEFAULT_PY_EXECUTABLE = PY_EXECUTABLES.SYSTEM
+
+    def __init__(self):
+        logging.getLogger("ifeval_verify").setLevel(logging.CRITICAL)
+
+    def verify(
+        self,
+        pred_responses: List[str],
+        prompts: List[str],
+        metadata: List[IFEvalEnvironmentMetadata],
+    ) -> List[float]:
+        """Verify the correctness of the predicted responses against the ground truth.
+
+        Args:
+            pred_responses: List[str]. The predicted responses from the LLM.
+            prompts: List[str]. The prompts that were used to generate the responses.
+            metadata: List[IFEvalEnvironmentMetadata]. The metadata for the prompts.
+
+        Returns:
+            List[float]. The rewards for each predicted response.
+        """
+        results = []
+        for response, prompt, metadata in zip(pred_responses, prompts, metadata):
+            try:
+                # with _mute_output():
+                try:
+                    ret_score, _ = instruction_following_rewards(
+                        prompt, response, metadata
+                    )
+                except Exception as e:
+                    print(
+                        f"Error in instruction_following_rewards: {e}, prompt: {prompt}, response: {response}, metadata: {metadata}"
+                    )
+                    ret_score = 0.0
+
+                results.append(float(ret_score))
+            except Exception:
+                results.append(0.0)
+        return results
+
+
+@ray.remote
+class IFEvalEnvironment(EnvironmentInterface):
+    DEFAULT_PY_EXECUTABLE = PY_EXECUTABLES.SYSTEM
+
+    def __init__(self, cfg: IFEvalEnvConfig):
+        self.cfg = cfg
+        self.num_workers = cfg["num_workers"]
+        self.workers = [
+            HFVerifyWorker.options(
+                runtime_env={"py_executable": HFVerifyWorker.DEFAULT_PY_EXECUTABLE}
+            ).remote()
+            for _ in range(self.num_workers)
+        ]
+
+    def shutdown(self):
+        # shutdown all workers
+        for worker in self.workers:
+            ray.kill(worker)
+
+    def step(
+        self,
+        message_log_batch: List[List[Dict[str, str]]],
+        metadata: List[IFEvalEnvironmentMetadata],
+    ) -> EnvironmentReturn:
+        """Runs a step in the math environment.
+
+        Args:
+            message_log: List[List[Dict[str, str]]]. A batch of OpenAI-API-like message logs that represent interactions with the LLM.
+            metadata: List[IFEvalEnvironmentMetadata]. The grader will use the 'ground_truth' key to evaluate correctness.
+
+        Returns:
+            EnvironmentReturn: A tuple containing:
+                - List[Dict[str, str]]: Observations/responses batch
+                - List[Dict]: Updated metadata
+                - List[str]: Next stop strings for the next turn
+                - Tensor: Rewards tensor
+                - Tensor: Done flags tensor
+        """
+        # Extract the assistant's responses from the message history
+        # Each message list should have at least one assistant response
+        assistant_response_batch = []
+        prompts = []
+        for conversation in message_log_batch:
+            assistant_responses = [
+                interaction["content"]
+                for interaction in conversation
+                if interaction["role"] == "assistant"
+            ]
+            assistant_response_batch.append(assistant_responses[-1])
+            prompts.append(conversation[0]["content"])
+
+        chunked_assistant_response_batch = chunk_list_to_workers(
+            assistant_response_batch, self.num_workers
+        )
+        chunked_prompts = chunk_list_to_workers(prompts, self.num_workers)
+        chunked_metadata = chunk_list_to_workers(metadata, self.num_workers)
+
+        # # Process each chunk in parallel
+        futures = [
+            self.workers[i].verify.remote(response, prompt, metadata)
+            for i, (response, prompt, metadata) in enumerate(
+                zip(chunked_assistant_response_batch, chunked_prompts, chunked_metadata)
+            )
+        ]
+
+        results = ray.get(futures)
+
+        # flatten the results
+        results = [item for sublist in results for item in sublist]
+        observations = [
+            {
+                "role": "environment",
+                "content": "Environment: correct"
+                if result
+                else "Environment: incorrect",
+            }
+            for result in results
+        ]
+
+        # create a tensor of rewards and done flags
+        rewards = torch.tensor(results).cpu()
+        done = torch.ones_like(rewards).cpu()
+
+        next_stop_strings = [None] * len(message_log_batch)
+
+        return EnvironmentReturn(
+            observations=observations,
+            metadata=metadata,
+            next_stop_strings=next_stop_strings,
+            rewards=rewards,
+            terminateds=done,
+        )
+
+    def global_post_process_and_metrics(
+        self, batch: BatchedDataDict
+    ) -> Tuple[BatchedDataDict, dict]:
+        """Computes metrics for this environment given a global rollout batch.
+
+        Every rank will run this function, so you're free to use distributed
+        calculations if you'd prefer for heavy metrics.
+        """
+        batch["rewards"] = (
+            batch["rewards"] * batch["is_end"]
+        )  # set a reward of 0 for any incorrectly ended sequences
+        if (batch["rewards"] == 1).float().sum() > 0:
+            correct_solution_generation_lengths = (
+                (batch["generation_lengths"] - batch["prompt_lengths"])[
+                    batch["rewards"] == 1
+                ]
+                .float()
+                .mean()
+                .item()
+            )
+        else:
+            correct_solution_generation_lengths = 0
+
+        metrics = {
+            # "table": table, TODO @sahilj WIP
+            "accuracy": batch["rewards"].mean().item(),
+            "pass@samples_per_prompt": calculate_pass_rate_per_prompt(
+                batch["text"], batch["rewards"]
+            ),
+            "fraction_of_samples_properly_ended": batch["is_end"].float().mean().item(),
+            "num_problems_in_batch": batch["is_end"].shape[0],
+            "generation_lengths": batch["generation_lengths"].float().mean().item(),
+            "prompt_lengths": batch["prompt_lengths"].float().mean().item(),
+            "correct_solution_generation_lengths": correct_solution_generation_lengths,
+        }
+
+        return batch, metrics
