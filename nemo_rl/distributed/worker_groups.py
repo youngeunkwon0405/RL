07d3b6e108530653efffe1fcc0d9f8a58441f942
Yi-Fu Wu <yifu.wu@gmail.com>
2025-05-25 01:26:13 -0700

    Fixes
    - get_logprobs for MegatronPolicyWorker needs to be passed as kwarg. Related: https://github.com/ray-project/ray/issues/26283
    - need old numpy version
    
    Signed-off-by: Yi-Fu Wu <yifu.wu@gmail.com>

--- nemo_rl/distributed/worker_groups.py
+++ nemo_rl/distributed/worker_groups.py
@@ -630,7 +630,7 @@ class RayWorkerGroup:
 
                 # Call the method on the worker with its data slice
                 future = getattr(worker, method_name).remote(
-                    worker_data, **common_kwargs
+                    data=worker_data, **common_kwargs
                 )
                 futures.append(future)
                 called_workers.append(worker_idx)
@@ -638,7 +638,7 @@ class RayWorkerGroup:
                 # If this worker doesn't need data:
                 if make_dummy_calls_to_free_axes:
                     # If make_dummy_calls_to_free_axes is True, just call the method with None
-                    future = getattr(worker, method_name).remote(None, **common_kwargs)
+                    future = getattr(worker, method_name).remote(data=None, **common_kwargs)
                     futures.append(future)
                     called_workers.append(worker_idx)
                 else:
