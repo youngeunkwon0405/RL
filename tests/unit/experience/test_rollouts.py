914e76b6ccbe55b84c43e3fbc03c5f56848f2b1e
Parth Chadha <pchadha@nvidia.com>
2025-05-28 21:08:26 -0700

    feat: use async vllm engine (only used in unit tests) (#418)
    Signed-off-by: Parth Chadha <pchadha@nvidia.com>

--- tests/unit/experience/test_rollouts.py
+++ tests/unit/experience/test_rollouts.py
@@ -221,6 +221,7 @@ base_vllm_test_config: VllmConfig = {
     "stop_token_ids": None,
     "stop_strings": None,
     "vllm_cfg": {
+        "async_engine": False,
         "precision": "bfloat16",
         "tensor_parallel_size": 1,
         "max_model_len": 2048,
