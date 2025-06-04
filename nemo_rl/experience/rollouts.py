d59cc90ebd45a3614ef759f09b239c42eb518792
Parth Chadha <pchadha@nvidia.com>
2025-06-03 18:13:56 -0700

    Revert wrong commit
    Signed-off-by: Parth Chadha <pchadha@nvidia.com>

--- nemo_rl/experience/rollouts.py
+++ nemo_rl/experience/rollouts.py
@@ -61,14 +61,9 @@ def generate_responses(
         generation_input_data["stop_strings"] = [None] * len(input_lengths)
 
     # Generate responses
-    if policy_generation.cfg["vllm_cfg"]["async_engine"]:
-        generation_outputs = policy_generation.generate_async(
-            generation_input_data, greedy=greedy
-        )
-    else:
-        generation_outputs = policy_generation.generate(
-            generation_input_data, greedy=greedy
-        )
+    generation_outputs = policy_generation.generate(
+        generation_input_data, greedy=greedy
+    )
 
     # Extract generated tokens
     generated_ids = []
