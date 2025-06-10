e4b1d458fab91ef709ea00ed6df2c315e0565626
Yi-Fu Wu <yifu.wu@gmail.com>
2025-06-09 18:55:26 -0700

    Cleanup + fix split_keys
    Signed-off-by: Yi-Fu Wu <yifu.wu@gmail.com>

--- nemo_rl/models/policy/megatron_policy_worker.py
+++ nemo_rl/models/policy/megatron_policy_worker.py
@@ -1166,9 +1166,6 @@ class MegatronPolicyWorker:
                 (
                     (
                         name,
-                        tp_rank_ids,
-                        pp_rank_ids,
-                        ep_rank_ids,
                         tuple(shape),
                         param.dtype,
                     ),
@@ -1233,31 +1230,20 @@ class MegatronPolicyWorker:
         Returns:
             Dict mapping device UUID to list of (mapped_key, handle) tuples
         """
-        st_gather_params = time.time()
         gathered_megatron_params = gather_params(
             self.model,
             keys,
         )
-        print(f"TIME gather_params: {time.time() - st_gather_params}")
-        st_convert_params = time.time()
         gathered_hf_params = self.megatron_to_hf_converter.convert(
             gathered_megatron_params, self.model.config
         )
-        print(f"TIME convert_params: {time.time() - st_convert_params}")
 
-        # st_gc = time.time()
-        # gc.collect()
-        # torch.cuda.empty_cache()
-        # print(f"TIME gc: {time.time() - st_gc}")
-
-        st_rest = time.time()
         # Get device UUID for IPC handles
         device_uuid = self.report_device_id()
         from torch.multiprocessing.reductions import reduce_tensor
 
         # Create IPC handles for each parameter
         all_handles = []
-        st_reduce_tensor = time.time()
         for key, tensor in gathered_hf_params.items():
             handle = reduce_tensor(tensor.detach())
             all_handles.append((key, handle))
@@ -1268,8 +1254,6 @@ class MegatronPolicyWorker:
         for key, tensor in gathered_hf_params.items():
             shapes[key] = tensor.shape
 
-        print(f"TIME rest: {time.time() - st_rest}")
-        print("TIME total get_weights_ipc_handles: ", time.time() - st_gather_params)
         return {device_uuid: all_handles}
 
     def prepare_for_lp_inference(self):
