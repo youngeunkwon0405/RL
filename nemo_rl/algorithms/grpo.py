39fc46d145681c764f53f64476235e233e3b7fb7
Guyue Huang <guyueh@nvidia.com>
2025-06-12 13:41:44 -0700

    Add more time logs
    Signed-off-by: Guyue Huang <guyueh@nvidia.com>

--- nemo_rl/algorithms/grpo.py
+++ nemo_rl/algorithms/grpo.py
@@ -304,6 +304,10 @@ def refit_policy_generation(
 
     # Streaming update weights to save memory
     state_dict_info: list[tuple[str, int]] = policy.prepare_weights_for_ipc()
+    et = time.time()
+    print(f"[Refit] Total time taken to prepare_weights_for_ipc: {et - st}")
+    st = et
+
     # group keys to save time
     available_bytes = refit_buffer_size_gb * (1024**3)
     split_keys: list[list[str]] = []
@@ -320,9 +324,21 @@ def refit_policy_generation(
 
     if len(keys) > 0:
         split_keys.append(keys)
+    
+    et = time.time()
+    print(f"[Refit] Total time taken to split keys: {et - st}")
+    st = et
+
     # do update
-    for keys in split_keys:
+    total_get_weight_ipc_handle_time = 0
+    total_update_weights_time = 0
+    for i, keys in enumerate(split_keys):
         ipc_handles = policy.get_weights_ipc_handles(keys)
+        et = time.time()
+        print(f"[Refit] Total time taken to get ipc handles for {i}th split: {et - st}")
+        total_get_weight_ipc_handle_time += et - st
+        st = et
+
         if not policy_generation.update_weights(ipc_handles):
             error_message = (
                 "❌ Error: Updating weights for the generation policy failed during refit.\n"
@@ -330,10 +346,14 @@ def refit_policy_generation(
                 "a problem within the generation backend (e.g., vLLM worker).\n"
             )
             raise RuntimeError(error_message)
+        
+        et = time.time()
+        print(f"[Refit] Total time taken to update weights for {i}th split: {et - st}")
+        total_update_weights_time += et - st
+        st = et
     
-    et = time.time()
-    print(f"[Refit] Total time taken to update weights: {et - st}")
-    st = et
+    print(f"[Refit] Total time taken to get ipc handles: {total_get_weight_ipc_handle_time}")
+    print(f"[Refit] Total time taken to update weights: {total_update_weights_time}")
 
     policy.offload_after_refit()
     policy_generation.prepare_for_generation(tags=["kv_cache"])
