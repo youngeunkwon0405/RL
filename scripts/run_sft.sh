164ad51e8811ded842369bab9f1044b6e598416a
Zhaocheng Zhu <zhaochengz@nvidia.com>
2025-06-12 23:34:00 -0700

    update scripts to fit new ray cluster implementation
    Signed-off-by: Zhaocheng Zhu <zhaochengz@nvidia.com>

--- scripts/run_sft.sh
+++ scripts/run_sft.sh
@@ -2,12 +2,17 @@ export HF_TOKEN=YOUR_HF_TOKEN
 export WANDB_API_KEY=YOUR_WANDB_API_KEY
 export RAY_DEDUP_LOGS=0
 
-export CONTAINER="/home/zhaochengz/containers/ray+uv.sqsh"
-export COMMAND="cd /home/zhaochengz/lustre/reinforcer; uv pip install -e .; uv run examples/run_sft.py"
-# store experiment results in current working directory
-# recursively expand soft links in PWD
-WD=$(readlink -f $PWD)
-export MOUNTS="/home/zhaochengz/lustre/reinforcer:/home/zhaochengz/lustre/reinforcer,/home/zhaochengz/lustre/datasets:/home/zhaochengz/lustre/datasets,$WD:$WD"
+export CONTAINER="/home/zhaochengz/containers/nemo-rl.sqsh"
+export COMMAND="uv run examples/run_sft.py"
+# store experiment logs in BASE_LOG_DIR
+export BASE_LOG_DIR="/home/zhaochengz/lustre/experiments"
+# recursively expand soft links
+export MOUNTS="$(readlink -f /home/zhaochengz/lustre/reinforcer):$(readlink -f /home/zhaochengz/lustre/reinforcer),\
+$(readlink -f /home/zhaochengz/lustre/reinforcer):/home/zhaochengz/lustre/reinforcer,\
+$(readlink -f /home/zhaochengz/lustre/datasets):/home/zhaochengz/lustre/datasets,\
+$(readlink -f /home/zhaochengz/lustre/experiments):/home/zhaochengz/lustre/experiments"
 
-sbatch --nodes=2 --account=llmservice_modelalignment_ppo --job-name=sft --partition=batch --time=4:0:0 --gres=gpu:8 \
+sbatch --account=llmservice_modelalignment_ppo --job-name=sft \
+    --nodes=1 --partition=interactive --time=4:0:0 --gres=gpu:8 \
+    --output=${BASE_LOG_DIR}/slurm-%j.out \
     /home/zhaochengz/lustre/reinforcer/ray.sub
