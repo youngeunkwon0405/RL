fix: Don't call broadcast on dtensor  (#627)
Signed-off-by: Parth Chadha <pchadha@nvidia.com>

nemo_rl/models/policy/dtensor_policy_worker.py
@@ -263,7 +263,7 @@ class DTensorPolicyWorker:
 
         # Manually broadcast buffers
         for _, buf in self.model.named_buffers():
-            torch.distributed.broadcast(buf, src=0)
+            torch.distributed.broadcast(to_local_if_dtensor(buf), src=0)
 
         if self.cpu_offload:
             self.model = self.move_to_device(self.model, "cpu")