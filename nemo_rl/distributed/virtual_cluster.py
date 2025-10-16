8f6e00ef9d518c56ba81bde9978e534983de8148
Bogdan Salyp <bogdansalyp@gmail.com>
2025-10-15 19:49:58 -0700

    fix: Megatron worker to have locked dependencies (#1315)
    Signed-off-by: Bogdan Salyp <bogdansalyp@gmail.com>

--- nemo_rl/distributed/virtual_cluster.py
+++ nemo_rl/distributed/virtual_cluster.py
@@ -51,14 +51,11 @@ class PY_EXECUTABLES:
     # Use NeMo-RL direct dependencies and nemo-automodel.
     AUTOMODEL = "uv run --locked --extra automodel"
 
+    # Use NeMo-RL direct dependencies and Megatron.
+    MCORE = "uv run --locked --extra mcore"
+
     # Use Penguin dependencies
     PENGUIN = "uv run --locked --extra penguin"
-
-    # Megatron-core (and nemo dependencies)
-    # We always run with --reinstall to avoid issues where someone runs "uv run ... --extra mcore ..."
-    # but the submodules are not downloaded yet. This results in errors where it appears Megatron/Nemo
-    # aren't installed. Simple workaround is to always run the mcore py_executable with --reinstall.
-    MCORE = "uv run --reinstall --extra mcore"
 
 
 @ray.remote  # pragma: no cover
