2e2c2b334292f301a5eb57a17fd5ed63a54d5425
Yu Yao <54727607+yaoyu-33@users.noreply.github.com>
2025-11-04 03:39:52 -0800

    chore: Update RL to use megatron-bridge tot (#1358)
    Signed-off-by: Terry Kong <terryk@nvidia.com>
    Signed-off-by: Yu Yao <54727607+yaoyu-33@users.noreply.github.com>
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>
    Co-authored-by: Terry Kong <terryk@nvidia.com>

--- nemo_rl/models/policy/lm_policy.py
+++ nemo_rl/models/policy/lm_policy.py
@@ -96,6 +96,13 @@ class Policy(ColocatablePolicyInterface, GenerationInterface):
             cp_size = config["megatron_cfg"]["context_parallel_size"]
 
             env_vars = config["megatron_cfg"].get("env_vars", {})
+
+            if "TORCH_CUDA_ARCH_LIST" not in os.environ:
+                raise RuntimeError(
+                    "TORCH_CUDA_ARCH_LIST is not set. This is required in Megatron backend. This variable is set in our container, but "
+                    "if you are running a custom container or baremetal, you may need to set this variable manually. Example: export TORCH_CUDA_ARCH_LIST='9.0 10.0'"
+                )
+
         else:
             if not dtensor_enable:
                 raise ValueError(
